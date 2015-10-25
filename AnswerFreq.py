75dffc2e7c548fbf666dddff52319a359da4414e
VeniVidiVici42 <bogtro123@gmail.com>
2015-10-24 22:07:29 -0400

    Update AnswerFreq.py

--- AnswerFreq.py
+++ AnswerFreq.py
@@ -97,4 +97,4 @@ for i in range(2002,2016):
 	update(i, 10, 'B', answer_freq_10, letter_freq_10)
 
 print_12(answer_freq_12, letter_freq_12)
-print_10(answer_freq_12, letter_freq_12)
+print_10(answer_freq_10, letter_freq_10)
